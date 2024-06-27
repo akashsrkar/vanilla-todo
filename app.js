8b7517064cff8c67a7628c6e61fea6dcb94fd0ea
akashsrkar <akashsarkar.dev@gmail.com>
2024-06-27 13:28:08 +0530

    feature: add, remove and clearAll

--- app.js
+++ app.js
@@ -1,26 +1,59 @@
 const form = document.querySelector("form");
+const list = document.querySelector("#todo-list");
+const clearAll = document.querySelector("#clear");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    let input = document.querySelector("input");
-    if(input.value !== ""){
-        addTodo(input.value);
-        input.value = "";
-    }
+  let input = document.querySelector("input");
+  if (input.value !== "") {
+    addTodo(input.value);
+    input.value = "";
+  }
 });
 
+function addTodo(todo) {
+  const list = document.querySelector("#todo-list");
+  const item = document.createElement("li");
 
-function addTodo(todo){
-    const list = document.querySelector("#todo-list");
-    const item = document.createElement("li");
-
-    item.innerHTML = `
+  item.innerHTML = `
     <span>${todo}</span>
     <div class="button-container">
-    <button class="todo-button"><i class="ph ph-check-fat"></i></button>
-    <button class="todo-button"><i class="ph ph-trash"></i></button>
+    <button name="check" class="todo-button"><i class="ph ph-check-fat"></i></button>
+    <button name="delete" class="todo-button"><i class="ph ph-trash"></i></button>
     </div>
-    `
-    list.appendChild(item);
+    `;
+  list.appendChild(item);
 }
+
+list.addEventListener("click", (e) => {
+  if (e.target.name === "check") {
+    checkTodo(e);
+    console.log("clicked");
+  } else if (e.target.name === "delete") {
+    deleteTodo(e);
+  }
+});
+
+function checkTodo(e) {
+  const item = e.target.parentNode.parentNode.children[0];
+
+  if (item.style.textDecoration === "line-through") {
+    item.style.textDecoration = "none";
+  } else {
+    item.style.textDecoration = "line-through";
+  }
+}
+
+function deleteTodo(e) {
+  const item = e.target.parentNode.parentNode;
+  console.log(item.classList);
+  item.addEventListener("transitionend", () => {
+    item.remove();
+  });
+  item.classList.add("transition-fall");
+}
+
+clearAll.addEventListener("click", () => {
+  list.innerHTML = "";
+});
